84cd96f2675e0273b17b1dcea05e5765ce2c2945
jperezsoy <jonatanperezsoy@gmail.com>
2015-03-22 03:39:37 -0600

    add utils baches2

---
+++ app/src/main/java/baches/jperez/soy/utils/ConstantsUtils.java
@@ -0,0 +1,12 @@
+package baches.jperez.soy.utils;
+
+/**
+ * Created by jonatan on 22/03/15.
+ */
+public class ConstantsUtils {
+
+    public static final String URL_ROOT_SOYBACHERO_API = "http://192.168.1.93:8001";
+    public static final String URL_AUTHENTICATION = URL_ROOT_SOYBACHERO_API + "/token-auth/";
+    public static final String URL_GETBACHES = URL_ROOT_SOYBACHERO_API + "/api/reporte/";
+
+}
